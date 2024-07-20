b0624353dd2c91d5aac36984cebb4f377596fab0
Divya Patidar <divyapatidar28178@gmail.com>
2024-07-20 12:45:19 +0530

    change

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: true,
     credentials: true,
     exposedHeaders: ["set-cookie", "token"],
     methods: ["GET", "POST", "PUT", "DELETE"],
