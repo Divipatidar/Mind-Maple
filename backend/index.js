6eaf69c763418dcc1899e5faa3102727d680caea
Divya Patidar <divyapatidar28178@gmail.com>
2024-08-02 16:08:19 +0530

    c

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ app.use(
     credentials: true,
     exposedHeaders: ["set-cookie", "token"],
     methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
 app.use(urlencoded({ extended: false }));
