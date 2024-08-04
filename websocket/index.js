e9ff31c08460b4fa679b428514561dea09a42b64
Divya Patidar <divyapatidar28178@gmail.com>
2024-08-04 18:00:25 +0530

    changes

--- websocket/index.js
+++ websocket/index.js
@@ -1,79 +1,75 @@
-const WebSocket = require("ws");
-const dotenv = require("dotenv");
-dotenv.config();
+const { WebSocketServer } = require('ws');
+require('dotenv').config();
 
-const port = process.env.PORT || 8802;
-const wss = new WebSocket.Server({ port: port });
+const port = `${process.env.PORT}` || 3001;
+const wss = new WebSocketServer({ port: port });
 
 const map = new Map();
-/* 
-    This is an inmemory storage for all the rooms
-    Map will store the room id of the current users and their connection
-    map type = {
-        uuid : Connection[]
-    }
-    connection type = {
-        connectId : number,
-        ws : WebSocket Connection
-    }
-*/
 
 let counter = 0;
-// wss.broadcast = (id, data) => {
-//   console.log("bcas");
-//   wss.clients.forEach((client) => {
-//     if (client.readyState == WebSocket.OPEN && data != undefined) {
-//       if (client.id === id) {
-//         client.send(data);
-//       }
-//     }
-//   });
-// };
-wss.on("connection", async (ws, req) => {
-  const params = new URLSearchParams(req.url.split("?")[1]);
-  const id = params?.get("id");
-  if (!id) {
-    // console.log("Invalid! no id");
-    ws.terminate();
-    return;
-  }
-  const isServer = params?.get("isServer") == "true";
 
-  if (!isServer && (!map.has(id) || map.get(id).server === undefined)) {
-    // console.log("Invalid! no server wrt id");
-    ws.terminate();
-    return;
-  }
+console.log(`WebSocket server is running on port ${port}`);
 
-  if (!map.has(id)) {
-    map.set(id, {});
-  }
+wss.on('connection', (ws, req) => {
+  console.log("WebSocket connection established"); // Add this line
+  const address = req.url;
+  console.log(`Incoming connection URL: ${address}`);
 
-  if (isServer) {
-    map.get(id).server = ws;
-  } else {
-    map.get(id).client = ws;
-  }
+  try {
+    const params = new URLSearchParams(req.url.split('?')[1]);
+    const id = params?.get('id');
+    console.log('New connection established');
+    console.log(`Client ID: ${id}`);
 
-  const connectId = counter++;
+    if (!id) {
+      console.log('Invalid connection: No ID provided');
+      ws.terminate();
+      return;
+    }
 
-  // ws["id"] = id;
+    const isServer = params?.get('isServer') === 'true';
+    console.log("websocket server",isServer)
 
-  ws.on("message", (data, isBinary) => {
-    const arr = map.get(id);
+    if (!isServer && (!map.has(id) || map.get(id).server === undefined)) {
+      console.log('Invalid connection: No server associated with this ID');
+      ws.terminate();
+      return;
+    }
+
+    if (!map.has(id)) {
+      map.set(id, {});
+    }
 
     if (isServer) {
-      map.get(id)?.client?.send(data, { binary: isBinary });
+      map.get(id).server = ws;
     } else {
-      map.get(id)?.server?.send(data, { binary: isBinary });
+      map.get(id).client = ws;
     }
-  });
-  ws.on("close", () => {
-    if (isServer) {
-      map.get(id)?.client?.terminate();
-    } else {
-      map.get(id)?.server?.terminate();
-    }
-    map.delete(id);
-  });
+
+    const connectId = counter++;
+
+    ws.on('message', (data, isBinary) => {
+      const arr = map.get(id);
+      console.log(`Message received from ID ${id}`);
+
+      if (isServer) {
+        map.get(id)?.client?.send(data, { binary: isBinary });
+      } else {
+        map.get(id)?.server?.send(data, { binary: isBinary });
+      }
+    });
+
+    ws.on('close', () => {
+      console.log(`Connection closed for ID ${id}`);
+      if (isServer) {
+        map.get(id)?.client?.terminate();
+      } else {
+        map.get(id)?.server?.terminate();
+      }
+      map.delete(id);
+    });
+  } catch (error) {
+    console.log(`Invalid URL: ${address}`);
+    console.error(error);
+  }
 });
