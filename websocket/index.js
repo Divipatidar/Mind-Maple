65fd12e075ccea026d9d7f3263f7371e909e1c8f
Divya patidar <divyapatidar28178@gmail.com>
2025-07-24 20:32:06 +0530

    changes

--- websocket/index.js
+++ websocket/index.js
@@ -1,7 +1,7 @@
 const { WebSocketServer } = require('ws');
 require('dotenv').config();
 
-const port = 3001;
+const port = 3001 || 5000;
 const wss = new WebSocketServer({ port: port });
 
 const map = new Map();
