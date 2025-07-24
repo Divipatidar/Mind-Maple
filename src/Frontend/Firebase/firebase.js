7a54dcf331638928eec5f2fd68f9d62c7ae78887
Divya patidar <divyapatidar28178@gmail.com>
2025-07-24 20:11:32 +0530

    changes

--- src/Frontend/Firebase/firebase.js
+++ src/Frontend/Firebase/firebase.js
@@ -72,24 +72,24 @@ async function SignupWithEmail(email, password) {
   try {
     const result = await createUserWithEmailAndPassword(auth, email, password);
     const user = result.user;
-    const token = await user.getIdToken();
+    const token = await user.getIdToken(); 
+    const headers = {
+      Authorization: `Bearer ${token}`,
+       withCredentials: true
+    };
 
     const response = await axios.post(
       "https://backend-server-chi-nine.vercel.app/signup",
       {},
-      {
-        withCredentials: true,
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-      }
+      { headers , withCredentials: true}
     );
 
+    
     if (response.data.token) {
       localStorage.setItem("authToken", response.data.token);
-      axios.defaults.headers.common["Authorization"] =
-        `Bearer ${response.data.token}`;
+      axios.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${response.data.token}`;
     }
 
     return { credential: user, token: response.data.token };
@@ -98,6 +98,5 @@ async function SignupWithEmail(email, password) {
     throw error;
   }
 }
-
 
 export { LoginWithGoogle, LoginWithEmail, SignupWithEmail };
